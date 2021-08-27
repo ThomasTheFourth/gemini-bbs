62233c73d87cae60e2d502f604e1b7c55a3eea30
Thomas Barnwell <thomas@krakenlabs.io>
2021-08-27 15:52:45 -0400

    Adding date to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_140539) do
+ActiveRecord::Schema.define(version: 2021_08_27_194853) do
 
   create_table "message_boards", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_140539) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
+    t.date "created_on"
   end
 
   create_table "users", force: :cascade do |t|
